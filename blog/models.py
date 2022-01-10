94deb05d18b875ac332a510e7f9a4c5f5ee171de
JungSoochang <tnckddl2468@gmail.com>
2022-01-11 00:17:30 +0900

    포스트 목록 페이지에 대표 이미지 출력

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
 
+    # 이미지 저장(head_image, blank=True는 있어도되고 없어도 된다는 뜻)
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # author = later
